1edf8b91bfda03571c6a52ed44cb36d7e940d51a
Simon Payne <simon@honestsoftware.co.uk>
2020-10-06 12:25:53 +0100

    Created REST service with Spring Initializer

---
+++ src/test/java/uk/co/honestsoftware/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.honestsoftware.f1000task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
